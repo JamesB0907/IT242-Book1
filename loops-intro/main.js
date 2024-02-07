8cf819a377aa8a9e2b2478df164b9a1e08004a59
James K Brooks <brooks94@mctc.edu>
2024-02-07 13:25:57 -0500

    Completed Loooooooops

---
+++ loops-intro/main.js
@@ -0,0 +1,17 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for (i = 0; i < cohortOneStudents.length; i++) {
+    console.log(cohortOneStudents[i])
+}
+
+const jamesInterests = ["Dungeons and Dragons", "Reading", "Video Games", "Board Games", "Coding"]
+
+const favoriteInterest = jamesInterests[0]
+
+for (i = 0; i < jamesInterests.length; i++){
+    if ("Dungeons and Dragons" === jamesInterests[i]) {
+        console.log(`My absolute favorite interest is ${jamesInterests[i]}`)
+    }
+    console.log(`One of my interests is ${jamesInterests[i]}`)
+}
+
