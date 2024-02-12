9e017a9e4a78b7ad724f0f07b6017ead30e2b74e
James K Brooks <brooks94@mctc.edu>
2024-02-12 14:08:55 -0500

    Queen Tribute Done

--- majesty/main.js
+++ majesty/main.js
@@ -4,7 +4,22 @@
 */
 
 const queens = [];
+const tributeChest = []
 
+const payTribute = ( tributeId, description, queenId) => {
+    const tributeObject =
+    {
+        id: tributeId,
+        queenId: queenId,
+        description: description
+    }
+    tributeChest.push(tributeObject)
+}
+
+payTribute(1, "Gold", 4)
+payTribute(2,"Gems", 3)
+payTribute(3, "Silver", 1)
+payTribute(4, "Platinum", 2)
 
 const createQueen = (queenId, queenName) => {
     const queenObject = {
@@ -23,7 +38,7 @@ createQueen(5, "Queen 4")
 
 
 console.log(queens)
-
+console.log(tributeChest)
 
 const hailTheQueen = (nameString) => {
     return `Hail Her Majesty, ${nameString}.`  // This function returns a string
@@ -34,3 +49,11 @@ for (const queenObject of queens) {
     const hailMessage = hailTheQueen(queenObject.name)  // What should be passed as an argument?
     console.log(hailMessage)
 };
+
+for (const queenObject of queens) {
+    for (const tributeObject of tributeChest) {
+        if (tributeObject.id === queenObject.id) {
+            console.log(`${queenObject.name} receives ${tributeObject.description}`)
+        }
+    }
+}
